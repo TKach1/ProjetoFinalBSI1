7d78cbd23de7f0cd8292b2b71930c473fbe47770
TKach1 <62523109+TKach1@users.noreply.github.com>
2022-06-10 13:30:39 -0300

    Added Files

---
+++ GameModes.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ORG 'X'
+#define VAZ '.'
+#define TAM 101
+
+void inicBlinker(char **m, int nL, int nC);
+void inicBloco(char **m, int nL, int nC);
+void inicSapo(char **m, int nL, int nC);
+void inicGlider(char **m, int nL, int nC);
+void inicLWSS(char **m, int nL, int nC);
